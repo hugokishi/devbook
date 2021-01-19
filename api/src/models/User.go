c9f7989ffd99579c43562d17d465294595171d4d
hugokishi <hugopacheco370@gmail.com>
2021-01-19 20:40:40 -0300

    Add validates for user

--- api/src/models/User.go
+++ api/src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User - Model for user
 type User struct {
@@ -10,4 +14,37 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+// Prepare - Call methods to format and validate recieved users
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+	user.format()
+	return nil
+}
+
+// validate - Validate user
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("The name is required")
+	}
+	if user.Nick == "" {
+		return errors.New("The nickname is required")
+	}
+	if user.Email == "" {
+		return errors.New("The email is required")
+	}
+	if user.Password == "" {
+		return errors.New("The password is required")
+	}
+	return nil
+}
+
+// Format - Format user (remove spaces)
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
