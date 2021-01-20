a826bb10bfdd61e99c24d247b9df4d0ac3c86234
hugokishi <hugopacheco370@gmail.com>
2021-01-20 15:00:36 -0300

    Add validations on publication

--- api/src/models/Publication.go
+++ api/src/models/Publication.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publication - Model for publication
 type Publication struct {
@@ -10,4 +14,31 @@ type Publication struct {
 	AuthorID  uint64    `json:"author_id,omitempty"`
 	Likes     uint64    `json:"likes"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+// Prepare - Call methods to format and validate recieved publications
+func (publication *Publication) Prepare() error {
+	if err := publication.validate(); err != nil {
+		return err
+	}
+
+	publication.format()
+	return nil
+}
+
+// validate - Validate Publication
+func (publication *Publication) validate() error {
+	if publication.Title == "" {
+		return errors.New("The title is required")
+	}
+	if publication.Content == "" {
+		return errors.New("The Content is required")
+	}
+	return nil
+}
+
+// Format - Format Publication (remove spaces)
+func (publication *Publication) format() {
+	publication.Title = strings.TrimSpace(publication.Title)
+	publication.Content = strings.TrimSpace(publication.Content)
 }
